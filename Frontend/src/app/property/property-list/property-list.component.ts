8b3f9ea91c5fdea843fc16ae91aafc93b2662ec3
Ashish <ashishtaware851@gmail.com>
2024-05-02 10:14:24 +0530

    Service Added

--- Frontend/src/app/property/property-list/property-list.component.ts
+++ Frontend/src/app/property/property-list/property-list.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
+import { HousingService } from 'src/app/services/housing.service';
+import { IProperty } from '../IProperty.interface';
 
 @Component({
   selector: 'app-property-list',
   templateUrl: './property-list.component.html',
   styleUrls: ['./property-list.component.css']
 })
-export class PropertyListComponent {
+export class PropertyListComponent implements OnInit{
+
+  propertyList: Array<IProperty> = [];
+
+  constructor( private housingService:HousingService)
+  { 
+
+  }
+
+  ngOnInit(): void {
+    this.housingService.getAllProperties().subscribe(
+      data =>{
+        this.propertyList = data;
+      },err =>{
+        console.log(err);
+      }  
+    )
+  }
 
 }
